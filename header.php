3842d257b096b63bb5ba73597854beea28a70f35
Tony <tzhang995@gmail.com>
2016-01-18 20:43:11 -0500

    header file

---
+++ header.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html>
+	<head>
+		<!--bootstrap-->
+		<meta lang="en">
+		<meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
+		<title>Header</title>
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+			<script> 
+			$(function(){
+			  $("#header").load("header.html"); 
+			  $("#footer").load("footer.html"); 
+			});
+		</script> 
+	</head>
+</script> 		
+	<head>
+	<div id="header">
+		<nav class="navbar navbar-default">
+			<div class="containder-fluid">
+				<div class="navbar-header">
+					<!-- # will be where our site will be-->
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigationbar">
+		               <span class="sr-only">Toggle navigation</span>
+		               <span class="icon-bar"></span>
+		               <span class="icon-bar"></span>
+		               <span class="icon-bar"></span>
+		            </button>
+		            <a class="navbar-brand" href="./index.html">Alarm Shame</a>
+				</div>
+
+
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="./shame.html">Shames</a></li>
+					<li><a href="./about.html">About</a></li>
+				</ul>
+			</div> 
+		</nav>
+		</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
