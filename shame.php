229bd3ee51033244d03506aa5fd24518ea70e0ed
Tony <tzhang995@gmail.com>
2016-01-19 21:20:37 -0500

    shame page

---
+++ shame.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta lang="en">
+		<meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
+		<title>AlarmShame</title>
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+
+	</head>
+	<body>
+		<?php include("header.php");?>
+		<div id="inner">
+			<p class="text-center">THis is the shame page</p>
+		</div>
+
+
+
+
+		<div id="footer">
+			<p class="text-center">Alarm Shame, where 1 alarm will scare you to death</p>
+		</div>
+
+    	
+
+
+    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
