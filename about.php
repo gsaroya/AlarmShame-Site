fba2ae92690e8f3d8e802656b892ec55716aee1f
Gagan Saroya <sarogagan@gmail.com>
2016-02-02 20:54:31 -0500

    fixed css includes and collapsing nav-bar

--- about.php
+++ about.php
@@ -10,8 +10,7 @@
 		<title>AlarmShame</title>
 		<link rel="stylesheet" href="css/normalize.css">
 		<link rel="stylesheet" href="css/bootstrap.min.css">
-		<link rel="stylesheet" href="css/bootstrap-theme.css">
-		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/bootstrap-theme.min.css">
 		<link href='https://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
 		<link href='https://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="css/style.css">
--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 			<div class="containder-fluid">
 				<div class="navbar-header">
 					<!-- # will be where our site will be-->
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigationbar">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navigationbar">
 		               <span class="sr-only">Toggle navigation</span>
 		               <span class="icon-bar"></span>
 		               <span class="icon-bar"></span>
@@ -14,21 +14,22 @@
 		            <a class="navbar-brand" href="./index.php">Alarm Shame</a>
 				</div>
 
-				<ul class="nav navbar-nav navbar-right">
-					<li id="index" class="
-						<?php if($currentPage == "index") echo "active"; ?>">
-						<a href="./index.php">Home</a>
-					</li>
-					<li id="shame" class="
-						<?php if($currentPage == "shame") echo "active"; ?>">
-						<a href="./shame.php">Shames</a>
-					</li>
-					<li id="about" style="padding-right:20px;" class="
-						<?php if($currentPage == "about") echo "active"; ?>">
-						<a href="./about.php">About</a>
-					</li>
-				</ul>
-
+				<div class="collapse navbar-collapse" id="navigationbar">
+					<ul class="nav navbar-nav navbar-right">
+						<li id="index" class="
+							<?php if($currentPage == "index") echo "active"; ?>">
+							<a href="./index.php">Home</a>
+						</li>
+						<li id="shame" class="
+							<?php if($currentPage == "shame") echo "active"; ?>">
+							<a href="./shame.php">Shames</a>
+						</li>
+						<li id="about" style="padding-right:20px;" class="
+							<?php if($currentPage == "about") echo "active"; ?>">
+							<a href="./about.php">About</a>
+						</li>
+					</ul>
+				</div>
 			</div> 
 		</nav>
 	</div>
