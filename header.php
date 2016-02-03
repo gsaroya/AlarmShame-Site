21e6cbc2689e0986baaf3d4b131183d4d85983bc
Gagan Saroya <sarogagan@gmail.com>
2016-02-02 20:27:29 -0500

    Fixed header

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 		               <span class="icon-bar"></span>
 		               <span class="icon-bar"></span>
 		            </button>
-		            <a class="navbar-brand" href="./index.html">Alarm Shame</a>
+		            <a class="navbar-brand" href="./index.php">Alarm Shame</a>
 				</div>
 
 				<ul class="nav navbar-nav navbar-right">
