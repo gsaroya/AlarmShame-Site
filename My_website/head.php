29988fac66236b1e345423aabe95944744dba2d5
Tony <tzhang995@gmail.com>
2016-02-08 19:38:09 -0500

    Personal website testing area

---
+++ My_website/head.php
@@ -0,0 +1,9 @@
+<meta charset="UTF-8"/>
+<title>Tony (Chen) Zhang</title>
+<meta lang="en">
+<meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
+<title>AlarmShame</title>
+<link rel="stylesheet" href="css/normalize.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="css/style.css">
---
+++ My_website/index.php
@@ -0,0 +1,24 @@
+<?php $currentPage = "home"; ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include("head.php"); ?>
+	</head>
+
+	<body>
+		<?php include("header.php");?>
+		<!--Body paragraph-->
+		<div id="inner" class="container">
+			<div class="text-center page-header">
+				<h1>Tony (Chen) Zhang</h1>
+			</div>
+			<div class="body-paragraph">
+				<p class="text-center">
+					Data structures
+				</p>
+			</div>
+		</div>
+
+	</body>
+
+</html> 
