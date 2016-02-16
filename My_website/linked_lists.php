cb556e9d456413cfacc71bf9d2bf26f53fd0cf15
Tony <tzhang995@gmail.com>
2016-02-15 19:38:10 -0500

    linked-list

--- My_website/linked_lists.php
+++ My_website/linked_lists.php
@@ -52,8 +52,59 @@ int main(){
 }
                 </code></pre>
                 <p>
-Now lets say you want an insert function where you given the list and a value that you want to insert.
+Now lets say you want an insert function where you given the list and a value that you want to insert.Now lets say you want an insert function where you given the list and a value that you want to insert.
+Now there could be several ways for you to insert another value to the list. You could add it to the front (first) of the list, back (last) of the list or if you are really daring, insert in order. Depending on what you need, there are advantages and disadvantages for each type of insert, but we will talk about that later. The method is simple for insertion in a list, create a new instance and put it where you want it to be.
+Here is the insert function for inserting the new value into the back of the list.</p>
+				<pre class="prettyprint lang-cpp text-left"><code class="language-cpp">
+Node * insertBack(int val, Node * list){
+   //Create a new node but instead of making the next NULL, you make it the rest of the list
+   Node * n = new Node(val, list);
+   //Now all you need to know is return the new start of the list
+    return n;
+}</code></pre>
+				<p>
+Analysis: This will always be in constant time (O(1)) because we know where the back of the list is. So the only thing we need to do is point there and tell people that we are the new back of the list. Now there is a problem with this code and that is what happens if 2 different values sees the back of the list. Ex:
+				<pre class="prettyprint lang-cpp text-left"><code class="language-cpp">
+Node * originalLine = new Node(2,NULL);
+Node * lineOne = new Node(3,originalLine);
+Node * lineTwo = new Node(4,originalLine);
+				</code></pre>
+<p> As you can see, the line DIVERGED into 2 lines!!! That is not a proper line nor is that a proper linked list. There is a way to fix this but I'll talk about that later.</p>
 
+<p>The next type of insert is where you insert the new instance in order with a parameter of your choosing. Here is an example, pretend you are in grade 2 again. It is picture day and they want you to line up based on your height. The new instance is you trying to jump into the line and the parameter is the height of all the students. Here is the code for this type of insertion:</p>
+
+				<pre class="prettyprint lang-cpp text-left"><code class="language-cpp">
+Node * insertOrder(int val, Node * list){
+   //Need to check if the you are the first one in the list
+   If(list == NULL){
+      return new Node(val, NULL);
+   } 
+   //Now we need to check to see if you are smaller than the last value in that list
+   elseif (val <= list->val){
+      //Because you are smaller, everything past this point will be bigger than you, thus you stop here, this case should only happen on the first call when you are the smallest value in that list
+      return new Node(val, list);
+   }
+   //Now you check to see if you are biggest value in that list by checking to see if there are anymore in front of you
+   elseif (list->next == NULL){
+      //Because there is nothing bigger than you, you tell the current biggest thing that you are bigger
+      list->next = new Node(val, NULL);
+      //You have to return NULL because you did not change who the last person is
+      //Note: you would have to make an if condition with this call to tell whoever is calling it that the smallest has not changed
+      return NULL;
+   }
+   //Now you need to check to see if you are bigger than the current node but smaller than the node after that
+   elseif (val > list->val && val <= list->next->val){
+      //Since you know that you are between these two values, all we need to do now is create a new instance between these values
+      list->next = new Node(val,list->next);
+      return NULL;
+   }
+   //Else move to the next node because the value does not belong here
+   else {
+      return insertOrder(val,list->next);
+   }
+}
+				</code></pre>
+				
 			</div>
 		</div>
 
