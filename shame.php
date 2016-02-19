a8f39c1e6687a8b130007427c311f8029471688c
Tony <tzhang995@gmail.com>
2016-02-18 22:34:49 -0500

    sorting table

--- shame.php
+++ shame.php
@@ -68,4 +68,5 @@
     	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
 	</body>
+	<?php include("tablesort.php");?>
 </html>
