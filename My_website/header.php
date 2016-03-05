5348847560f2493af65a75c2d82e325b9fac8ca6
Tony <tzhang995@gmail.com>
2016-03-05 15:53:45 -0500

    personal website

--- My_website/header.php
+++ My_website/header.php
@@ -1,35 +1,27 @@
 <header>
-	<div id="header">
-		<nav class="navbar navbar-inverse">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navigationbar">
-		               <span class="sr-only">Toggle navigation</span>
-		               <span class="icon-bar"></span>
-		               <span class="icon-bar"></span>
-		               <span class="icon-bar"></span>
-		            </button>
-		            <a class="navbar-brand" href="./index.php">Tony Zhang</a>
-		        </div>
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+	            <a class="navbar-brand" href="./index.php">Tony Zhang</a>
+	        </div>
 
-		        <!--Possible tabs-->
-		        <div class="collapse navbar-collapse" id="navigationbar">
-		        	<ul class="nav navbar-nav navbar-right">
-		        		<li id="index" class="
-							<?php if($currentPage == "index") echo "active"; ?>">
-							<a href="./index.php">Home</a>
-						</li>
-		        		<li id="data-structures" class="
-							<?php if($currentPage == "data-structures") echo "active"; ?>">
-		        			<a href="./data_structures.php">Data Structures</a>
-		        		</li>
-		        		<li id="thanks"  class="
-							<?php if($currentPage == "thanks") echo "active"; ?>">
-		        			<a href="./thanks.php">Thanks</a>
-		        		</li>
-		        	</ul>
-		        </div>
-		    </div>
-		</nav>
-	</div>
+	        <!--Possible tabs-->
+	        <div class="" id="navigationbar">
+	        	<ul class="nav navbar-nav navbar-right">
+	        		<li id="index" class="
+						<?php if($currentPage == "index") echo "active"; ?>">
+						<a href="./index.php">Home</a>
+					</li>
+	        		<li id="data-structures" class="
+						<?php if($currentPage == "data-structures") echo "active"; ?>">
+	        			<a href="./data_structures.php">Data Structures</a>
+	        		</li>
+	        		<li id="thanks"  class="
+						<?php if($currentPage == "thanks") echo "active"; ?>">
+	        			<a href="./thanks.php">Thanks</a>
+	        		</li>
+	        	</ul>
+	        </div>
+	    </div>
+	</nav>
 </header>
