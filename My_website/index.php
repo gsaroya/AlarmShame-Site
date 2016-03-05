90b71b1de9114d0b2e9372219ee4679998d1cc16
Tony <tzhang995@gmail.com>
2016-03-05 13:12:52 -0500

    personal website

--- My_website/index.php
+++ My_website/index.php
@@ -20,8 +20,30 @@
 				</div>
 				<div class="container pLanguage">
 					<div class="proficient">
+						<p class="title">Proficient in:</p>
+						<ul>
+							<li>C++</li>
+							<li>Python</li>
+							<li>HTML/CSS</li>
+							<li>PHP</li>
+							<li>Scheme</li>
+						</ul>
+					</div>
+					<div class="familiar">
+						<p class="title">Familiar with:</p>
+						<ul>
+							<li>Java</li>
+							<li>JavaScript</li>
+							<li>MySQL</li>
+							<li>Ruby on Rails</li>
+							<li>Objective-C</li>
+						</ul>
 					</div>
 					<div class="technology">
+						<p class="title">Technologies worked with:</p>
+						<ul>
+							<li></li>
+						</ul>
 					</div>
 				</div>
 			</div>
