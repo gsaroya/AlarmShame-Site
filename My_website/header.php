c1056683f5e6effd55242643c7a30a3ffa1927ec
Tony <tzhang995@gmail.com>
2016-03-05 17:08:22 -0500

    personal website

--- My_website/header.php
+++ My_website/header.php
@@ -1,27 +1,22 @@
 <header>
-	<nav class="navbar navbar-inverse">
-		<div class="container-fluid">
-			<div class="navbar-header">
-	            <a class="navbar-brand" href="./index.php">Tony Zhang</a>
-	        </div>
 
-	        <!--Possible tabs-->
-	        <div class="" id="navigationbar">
-	        	<ul class="nav navbar-nav navbar-right">
-	        		<li id="index" class="header-tab
-						<?php if($currentPage == "index") echo "active"; ?>">
-						<a href="./index.php">Home</a>
-					</li>
-	        		<li id="data-structures" class="header-tab
-						<?php if($currentPage == "data-structures") echo "active"; ?>">
-	        			<a href="./data_structures.php">Data Structures</a>
-	        		</li>
-	        		<li id="thanks"  class="header-tab
-						<?php if($currentPage == "thanks") echo "active"; ?>">
-	        			<a href="./thanks.php">Thanks</a>
-	        		</li>
-	        	</ul>
-	        </div>
-	    </div>
-	</nav>
+	<div class="header-title">
+		<a class="" href="./index.php">Tony Zhang</a>
+	</div>
+	<div class="header-tab">
+		<ul class="">
+			<li id="index" class="
+				<?php if($currentPage == "index") echo "active"; ?>">
+				<a href="./index.php">Home</a>
+			</li>
+			<li id="data-structures" class="
+				<?php if($currentPage == "data-structures") echo "active"; ?>">
+				<a href="./data_structures.php">Data Structures</a>
+			</li>
+			<li id="thanks"  class="
+				<?php if($currentPage == "thanks") echo "active"; ?>">
+				<a href="./thanks.php">Thanks</a>
+			</li>
+		</ul>
+	</div>
 </header>
