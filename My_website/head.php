a3a513b2db534e0e63a08d2f363145995f6f6611
Tony <tzhang995@gmail.com>
2016-03-05 15:46:20 -0500

    personal website

--- My_website/head.php
+++ My_website/head.php
@@ -6,5 +6,6 @@
 <link rel="stylesheet" href="css/normalize.css">
 <link rel="stylesheet" href="css/bootstrap.min.css">
 <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="css/bootstrap.css">
 <link rel="stylesheet" href="css/style.css">
 <script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>
