06ae4ff0472baf6ae6d200e6ece614cf93d918b1
Tony <tzhang995@gmail.com>
2016-03-05 21:52:07 -0500

    new website format

--- My_website/header.php
+++ My_website/header.php
@@ -1,20 +1,20 @@
 <header class="header">
 	<div class="header-title">
-		<h1>
+		<h1 class="highlight">
 			<a href="./index.php">Tony Zhang</a>
 		</h1>
 	</div>
 	<div class="header-nav">
 		<ul class="header-tabs">
-			<li id="experiences" class="
+			<li id="experiences" class="highlight
 				<?php if($currentPage == "experiences") echo "active"; ?>">
 				<a href="./experiences.php">Experiences</a>
 			</li>
-			<li id="projects" class="
+			<li id="projects" class="highlight
 				<?php if($currentPage == "projects") echo "active"; ?>">
 				<a href="./projects.php">Projects</a>
 			</li>
-			<li id="notes"  class="
+			<li id="notes"  class="highlight
 				<?php if($currentPage == "notes") echo "active"; ?>">
 				<a href="./notes.php">Notes</a>
 			</li>
