b4fbf0f17b527a67f24d6bcec3d1644a746f472e
Gagan Saroya <sarogagan@gmail.com>
2016-03-12 13:14:13 -0500

    Added page management in server side

--- tablesort.php
+++ tablesort.php
@@ -5,46 +5,23 @@
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	}
+
+	// The default sorting
 	$field='name';
 	$sort='ASC';
+	$page = 1;
 
-	if(isset($_GET['sorting']))
-	{
-		if($_GET['sorting']=='ASC')
-		{
-		$sort='DESC';
+	if(isset($_GET['sorting'])) {
+		$sort = mysql_real_escape_string($_GET['sorting']) == 'DESC' ? 'DESC' : 'ASC';
+	}
+
+	if(isset($_GET['name'])) {
+		$field = mysql_real_escape_string($_GET['sorting']);
+		if($field != 'name' || $field != 'shame' || $field != 'location') {
+			$field = 'name';
 		}
-		else
-		{
-		$sort='ASC';
-		}
 	}
 
-	if($_GET['field']=='name')
-	{
-	   $field = "name";
-	}
-	elseif($_GET['field']=='shame')
-	{
-	   $field = "shame";
-	}
-	elseif($_GET['field']=='location')
-	{
-	   $field="location";
-	}
-
-	$sql = "SELECT name,shame,location FROM shames ORDER BY $field $sort";
-	$result = $conn->query($sql);
-	echo'<thead><tr><th><a href="./shame.php?sorting='.$sort.'&field=name">Name</a></th>
-     <th><a href="./shame.php?sorting='.$sort.'&field=shame">Shame</a></th>
-<th><a href="./shame.php?sorting='.$sort.'&field=location">Location</a></th></tr><thead><tbody>';
-  while($row = $result->fetch_assoc()) {
-		echo "<tr>";
-    	echo "<td>" . $row["name"] . "</td>";
-    	echo "<td>" . $row["shame"] . "</td>";
-    	echo "<td>" . $row["location"] . "</td>";
-    	echo "</tr>";
-	}
 	// fetch the page count
 	$limit = 10;
 	$result_count = $conn->query("select count(1) FROM shames;");
@@ -56,8 +33,33 @@
 	if (($total <= $limit) || ($total % $limit)) {
 		++$numPages;
 	}
+
+	if(isset($_GET['page'])) {
+		$page = ($_GET['page']);
+		if($page < 1 ) {
+			$page = 1;
+		} ($page > $numPages) {
+			$page = $numPages;
+		}
+	}
+
+	$sql = "SELECT name,shame,location FROM shames ORDER BY $field $sort LIMIT $limit*$page";
+	$result = $conn->query($sql);
+	echo'<thead>
+		<tr><th><a href="./shame.php?sorting='.$sort.'&field=name">Name</a></th>
+		<th><a href="./shame.php?sorting='.$sort.'&field=shame">Shame</a></th>
+		<th><a href="./shame.php?sorting='.$sort.'&field=location">Location</a></th></tr>
+	<thead><tbody>';
+  while($row = $result->fetch_assoc()) {
+		echo "<tr>";
+    	echo "<td>" . $row["name"] . "</td>";
+    	echo "<td>" . $row["shame"] . "</td>";
+    	echo "<td>" . $row["location"] . "</td>";
+    	echo "</tr>";
+	}
+
 	echo $numPages . " pages";
-		
+
 	echo'</tbody>';
 	$conn->close();
 ?>
