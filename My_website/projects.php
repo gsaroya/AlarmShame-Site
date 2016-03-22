5f4f19817ded99e6c2798f21d89213b8b94238b4
Tony <tzhang995@gmail.com>
2016-03-21 21:33:23 -0400

    Projects

--- My_website/projects.php
+++ My_website/projects.php
@@ -12,10 +12,41 @@
 
 		<!--Body paragraph-->
 		<div id="inner" class="container">
-			<div class="body-paragraph">
-				<p class="text-center">
-					
-				</p>
+			<div class="myList">
+				<div class="myListItem">
+					<div class="myListTitle">
+						Discover
+					</div>
+					<div class="myListSummary">
+						<ul>
+							<li>Did this</li>
+							<li>Did That</li>
+						</ul>
+					</div>
+					<div class="myListLanguages">
+						<ul>
+							<li>Firebase</li>
+							<li>Pebble</li>
+						</ul>
+					</div>
+				</div>
+				<div class="myListItem">
+					<div class="myListTitle">
+						AlarmShame
+					</div>
+					<div class="myListSummary">
+						<ul>
+							<li>Did this</li>
+							<li>Did That</li>
+						</ul>
+					</div>
+					<div class="myListLanguages">
+						<ul>
+							<li>Firebase</li>
+							<li>Pebble</li>
+						</ul>
+					</div>
+				</div>
 			</div>
 		</div>
 
