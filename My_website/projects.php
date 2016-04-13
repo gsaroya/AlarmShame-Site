71631ddd9b25d3c6d3f15ad93b8d0e564e565fd7
Tony Zhang <tzhang995@gmail.com>
2016-04-12 22:25:46 -0400

    Github link

--- My_website/projects.php
+++ My_website/projects.php
@@ -14,25 +14,27 @@
 		<div id="inner">
 			<div class="myList">
 				<div class="myListItem" id="Discover">
-					<div class="myListTitle">
-						<h2>Discover</h2>
-					</div>
-					<div class="myListSummary">
-						<ul>
-							<li>Connected multiple</li>
-							<li>Did That</li>
-						</ul>
-					</div>
-					<div class="outside-links">
-						<h3><a href="http://www.discoverthis.xyz" class="text-center">Discover</a></h3>
+					<div class="myListText">
+						<div class="myListTitle">
+							<h2>Discover</h2>
+						</div>
+						<div class="myListSummary">
+							<ul>
+								<li>Connected multiple</li>
+								<li>Did That</li>
+							</ul>
+						</div>
+						<div class="outside-links">
+							<h3><a href="http://www.discoverthis.xyz" class="text-center">Discover</a></h3>
 
-    					<h3><a href="https://github.com/tzhang995/WearHacks_2016"><i class="fa fa-github-square"></i> GitHub</a></h3>
-					</div>
-					<div class="myListLanguages">
-						<ul class="things-used">
-							<li>Firebase</li>
-							<li>Pebble</li>
-						</ul>
+	    					<h3><a href="https://github.com/tzhang995/WearHacks_2016"><i class="fa fa-github-square"></i> GitHub</a></h3>
+						</div>
+						<div class="myListLanguages">
+							<ul class="things-used">
+								<li>Firebase</li>
+								<li>Pebble</li>
+							</ul>
+						</div>
 					</div>
 					<div class="myListLogo">
 						<img src="img/logo.png">
