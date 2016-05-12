fa64e80739ceef383412c85d9f510d5ccad9e641
Tony Zhang <tzhang995@gmail.com>
2016-05-11 21:59:01 -0400

    more css

--- My_website/projects.php
+++ My_website/projects.php
@@ -14,50 +14,54 @@
 		<div id="inner">
 			<div class="myList">
 				<div class="myListItem container-fluid" id="Discover">
-					<div class="myListText">
-						<div class="myListTitle">
-							<h2>Discover</h2>
-						</div>
-						<div class="myListSummary">
-							<p>A pebble smart watch application that alerts the user when you are getting closer to your target destination.</p>
-						</div>
-						<div class="outside-links">
-							<h4><a href="http://www.discoverthis.xyz" class="text-center">Discover</a></h4>
+					<div class="row">
+						<div class="myListText col-sm-9 col-md-9 col-lg-9">
+							<div class="myListTitle">
+								<h2>Discover</h2>
+							</div>
+							<div class="myListSummary">
+								<p>A pebble smart watch application that alerts the user when you are getting closer to your target destination.</p>
+							</div>
+							<div class="outside-links">
+								<h4><a href="http://www.discoverthis.xyz" class="text-center">Discover</a></h4>
 
-	    					<h4><a href="https://github.com/tzhang995/WearHacks_2016"><i class="fa fa-github-square"></i> GitHub</a></h4>
+		    					<h4><a href="https://github.com/tzhang995/WearHacks_2016"><i class="fa fa-github-square"></i> GitHub</a></h4>
+							</div>
+							<div class="myListLanguages">
+								<ul class="things-used">
+									<li>Firebase</li>
+									<li>Pebble</li>
+								</ul>
+							</div>
 						</div>
-						<div class="myListLanguages">
-							<ul class="things-used">
-								<li>Firebase</li>
-								<li>Pebble</li>
-							</ul>
+						<div class="myListLogo col-sm-3 col-md-3 col-lg-3">
+							<img src="img/logo.png">
 						</div>
-					</div>
-					<div class="myListLogo">
-						<img src="img/logo.png">
 					</div>
 				</div>
 				<div class="myListItem container-fluid" id="AlarmShame">
-					<div class="myListText">
-						<div class="myListTitle">
-							<h2>AlarmShame</h2>
-						</div>
-						<div class="myListSummary">
-							<p>An Android application that deletes your file and publically shames you if you were late from your alarm</p>
-						</div>
-						<div class="outside-links">
-							<h4><a href="http://devpost.com/software/alarm-shaming" class="text-center">Alarm Shame</a></h4>
+					<div class="row">
+						<div class="myListText col-sm-9 col-md-9 col-lg-9">
+							<div class="myListTitle">
+								<h2>AlarmShame</h2>
+							</div>
+							<div class="myListSummary">
+								<p>An Android application that deletes your file and publically shames you if you were late from your alarm</p>
+							</div>
+							<div class="outside-links">
+								<h4><a href="http://devpost.com/software/alarm-shaming" class="text-center">Alarm Shame</a></h4>
 
-	    					<h4><a href="https://github.com/tzhang995/Alarm-Clock"><i class="fa fa-github-square"></i> GitHub</a></h4>
+		    					<h4><a href="https://github.com/tzhang995/Alarm-Clock"><i class="fa fa-github-square"></i> GitHub</a></h4>
+							</div>
+							<div class="myListLanguages">
+								<ul class="things-used">
+									<li>Android Studios</li>
+									<li>Java</li>
+								</ul>
+							</div>
 						</div>
-						<div class="myListLanguages">
-							<ul class="things-used">
-								<li>Android Studios</li>
-								<li>Java</li>
-							</ul>
+						<div class="myListLogo col-sm-3 col-md-3 col-lg-3">
 						</div>
-					</div>
-					<div class="myListLogo">
 					</div>
 				</div>
 			</div>
