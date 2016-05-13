9f66ac598627f77270cbecc09f9c90edd8eb2920
Tony Zhang <tzhang995@gmail.com>
2016-05-12 23:28:54 -0400

    even more css

--- My_website/experiences.php
+++ My_website/experiences.php
@@ -14,35 +14,43 @@
 		<div id="inner" class="container">
 			<div class="myList">
 				<div class="myListItem">
-					<div class="myListTitle">
-						Agile Test Engineer
-					</div>
-					<div class="myListSummary">
-						<p>A pebble smart watch application that alerts the user when you are getting closer to your target destination.</p>
-					</div>
-				</div>
-				<div class="myListItem">
-					<div class="myListTitle">
-						Top 5 at Wearhacks 2016
-					</div>
-					<div class="myListSummary">
-						<p>A pebble smart watch application that alerts the user when you are getting closer to your target destination.</p>
+					<div class="row">
+						<div class="myListTitle">
+							Agile Test Engineer
+						</div>
+						<div class="myListSummary">
+							<p>A pebble smart watch application that alerts the user when you are getting closer to your target destination.</p>
+						</div>
 					</div>
 				</div>
 				<div class="myListItem">
-					<div class="myListTitle">
-						Google Games 2016 Participant
-					</div>
-					<div class="myListSummary">
-						<p>A pebble smart watch application that alerts the user when you are getting closer to your target destination.</p>
+					<div class="row">
+						<div class="myListTitle">
+							Top 5 at Wearhacks 2016
+						</div>
+						<div class="myListSummary">
+							<p>A pebble smart watch application that alerts the user when you are getting closer to your target destination.</p>
+						</div>
 					</div>
 				</div>
 				<div class="myListItem">
-					<div class="myListTitle">
-						Google Code Jam 2016 Participant
+					<div class="row">
+						<div class="myListTitle">
+							Google Games 2016 Participant
+						</div>
+						<div class="myListSummary">
+							<p>A pebble smart watch application that alerts the user when you are getting closer to your target destination.</p>
+						</div>
 					</div>
-					<div class="myListSummary">
-						<p>A pebble smart watch application that alerts the user when you are getting closer to your target destination.</p>
+				</div>
+				<div class="myListItem">
+					<div class="row">
+						<div class="myListTitle">
+							Google Code Jam 2016 Participant
+						</div>
+						<div class="myListSummary">
+							<p>A pebble smart watch application that alerts the user when you are getting closer to your target destination.</p>
+						</div>
 					</div>
 				</div>
 			</div>
