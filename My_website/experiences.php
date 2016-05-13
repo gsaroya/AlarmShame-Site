14f7090ac562ff660994aa4d3f1adb1708db0fa2
Tony Zhang <tzhang995@gmail.com>
2016-05-12 23:46:35 -0400

    even more css

--- My_website/experiences.php
+++ My_website/experiences.php
@@ -15,41 +15,49 @@
 			<div class="myList">
 				<div class="myListItem container-fluid">
 					<div class="row">
-						<div class="myListTitle">
-							Agile Test Engineer
-						</div>
-						<div class="myListSummary">
-							<p>A pebble smart watch application that alerts the user when you are getting closer to your target destination.</p>
-						</div>
-					</div>
-				</div>
-				<div class="myListItem container-fluid">
-					<div class="row">
-						<div class="myListTitle">
-							Top 5 at Wearhacks 2016
-						</div>
-						<div class="myListSummary">
-							<p>A pebble smart watch application that alerts the user when you are getting closer to your target destination.</p>
+						<div class="myListText myListText col-sm-9 col-md-9 col-lg-9">
+							<div class="myListTitle">
+								Agile Test Engineer
+							</div>
+							<div class="myListSummary">
+								<p>A pebble smart watch application that alerts the user when you are getting closer to your target destination.</p>
+							</div>
 						</div>
 					</div>
 				</div>
 				<div class="myListItem container-fluid">
 					<div class="row">
-						<div class="myListTitle">
-							Google Games 2016 Participant
-						</div>
-						<div class="myListSummary">
-							<p>A pebble smart watch application that alerts the user when you are getting closer to your target destination.</p>
+						<div class="myListText myListText col-sm-9 col-md-9 col-lg-9">
+							<div class="myListTitle">
+								Top 5 at Wearhacks 2016
+							</div>
+							<div class="myListSummary">
+								<p>A pebble smart watch application that alerts the user when you are getting closer to your target destination.</p>
+							</div>
 						</div>
 					</div>
 				</div>
 				<div class="myListItem container-fluid">
 					<div class="row">
-						<div class="myListTitle">
-							Google Code Jam 2016 Participant
+						<div class="myListText myListText col-sm-9 col-md-9 col-lg-9">
+							<div class="myListTitle">
+								Google Games 2016 Participant
+							</div>
+							<div class="myListSummary">
+								<p>A pebble smart watch application that alerts the user when you are getting closer to your target destination.</p>
+							</div>
 						</div>
-						<div class="myListSummary">
-							<p>A pebble smart watch application that alerts the user when you are getting closer to your target destination.</p>
+					</div>
+				</div>
+				<div class="myListItem container-fluid">
+					<div class="row">
+						<div class="myListText myListText col-sm-9 col-md-9 col-lg-9">
+							<div class="myListTitle">
+								Google Code Jam 2016 Participant
+							</div>
+							<div class="myListSummary">
+								<p>A pebble smart watch application that alerts the user when you are getting closer to your target destination.</p>
+							</div>
 						</div>
 					</div>
 				</div>
