189903ed0544ad5c5533d066b16417a3ff842126
Tony Zhang <tzhang995@gmail.com>
2016-05-13 01:00:34 -0400

    even more css

--- My_website/experiences.php
+++ My_website/experiences.php
@@ -19,6 +19,9 @@
 							<div class="myListTitle">
 								Agile Test Engineer - Pivotal Labs
 							</div>
+							<div class="myListTime">
+								January 2016 - May 2016
+							</div>
 							<div class="myListSummary">
 								<p>Implemented exploratory testing methodologies to ensure quality products</p>
 								<p>Wrote Automation test cases on multiple platforms such as Android, iOS and Web</p>
@@ -33,6 +36,9 @@
 							<div class="myListTitle">
 								Top 5 at Wearhacks 2016
 							</div>
+							<div class="myListTime">
+								March 2016
+							</div>
 							<div class="myListSummary">
 								<p>Designed and developed a pebble smart watch application that alerts the user when you are getting closer to your target destination.</p>
 							</div>
@@ -45,6 +51,9 @@
 							<div class="myListTitle">
 								Google Games 2016 Participant
 							</div>
+							<div class="myListTime">
+								April 2016
+							</div>
 							<div class="myListSummary">
 								<p>Lead a team to participate in a series of challenges created by Google</p>
 							</div>
@@ -56,6 +65,9 @@
 						<div class="myListText myListText col-sm-9 col-md-9 col-lg-9">
 							<div class="myListTitle">
 								Google Code Jam 2016 Participant
+							</div>
+							<div class="myListTime">
+								April 2016
 							</div>
 							<div class="myListSummary">
 								<p>Solved coding challenges online that was created by Google</p>
